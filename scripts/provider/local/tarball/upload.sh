60b57d9f02dae173d6777b050bcdfc25ce3cc121
Ovidiu Maja <ovi.dan89@gmail.com>
2017-07-15 18:30:04 +0300

    Consider BACKUP_VERSION in LOCAL provider

--- scripts/provider/local/tarball/upload.sh
+++ scripts/provider/local/tarball/upload.sh
@@ -5,5 +5,5 @@
 
 uploadTarball(){
   echo "Starting Copy Tarball from /tmp/$1 to ${LOCAL_SAVE_LOCATION}"
-  cp /tmp/$1 ${LOCAL_SAVE_LOCATION}/
+  cp /tmp/$1 ${LOCAL_SAVE_LOCATION}/${BACKUP_VERSION}_${1}
 }
